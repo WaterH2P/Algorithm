936755040075b91599b4d2ba3e29c6d61a4cef2c
H2P <hzp_h2p@qq.com>
2020-03-09 23:50:18 +0800

    ...

--- LeetCode/Algorithms/121 maxProfit.py
+++ LeetCode/Algorithms/121 maxProfit.py
@@ -2,12 +2,15 @@ class Solution:
     def maxProfit(self, prices) -> int:
         if prices == None or len(prices) <= 1:
             return 0
-        prices[0] = min(prices[0], prices[1])
-        prices[1] = prices[1] - prices[0]
+        if prices[1] < prices[0]:
+            prices[0] = prices[1]
+        prices[1] -= prices[0]
         for i in range(2, len(prices)):
             if prices[i] < prices[0]:
                 prices[0] = prices[i]
-            prices[1] = max(prices[1], prices[i] - prices[0])
+                continue
+            if prices[i] - prices[0] > prices[1]:
+                prices[1] = prices[i] - prices[0]
         return prices[1]
 
 s = Solution()
---
+++ LeetCode/Algorithms/39 combinationSum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def combinationSum(self, candidates, target: int):
+        res, i = [], 0
+        while i < len(candidates):
+            if candidates[i] >= target:
+                if candidates[i] == target:
+                    res.append([target])
+                candidates.pop(i)
+                i -= 1
+            i += 1
+        
+            
+                
