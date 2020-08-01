a93afc33412825e173560ded53e1f932a09d0994
H2P <hzp_h2p@qq.com>
2020-08-01 20:48:30 +0800

    每日一题题目

---
+++ LeetCode/501-750/632 smallestRange.js
@@ -0,0 +1,11 @@
+// 632. 最小区间
+// 你有 k 个升序排列的整数数组。找到一个最小区间，使得 k 个列表中的每个列表至少有一个数包含在其中。
+// 我们定义如果 b-a < d-c 或者在 b-a == d-c 时 a < c，则区间 [a,b] 比 [c,d] 小。
+
+/**
+ * @param {number[][]} nums
+ * @return {number[]}
+ */
+var smallestRange = function(nums) {
+
+};
